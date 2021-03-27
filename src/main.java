92b582689922be94b5c1ef92109363ff96a7d631
Dorlevi <dorlevi93@gmail.com>
2021-03-27 13:52:22 +0300

    MyMazeGenerator class --- DFS algo comment added

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import algorithms.mazeGenerators.*;
 
 public class main {
     public static void main(String[] args) {
-        Maze m = new MyMazeGenerator().generate(20,20);
+        Maze m = new MyMazeGenerator().generate(14,15);
         System.out.println(m);
     }
 }
