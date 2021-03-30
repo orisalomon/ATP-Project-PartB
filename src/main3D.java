5463e09b1f9ecf65e5bbce0d1ee0d484459c8a96
orisalomon <63231732+orisalomon@users.noreply.github.com>
2021-03-30 12:35:23 +0300

    changed Maze3D

---
+++ src/main3D.java
@@ -0,0 +1,8 @@
+import maze3D.Maze3D;
+
+public class main3D {
+
+    public static void main(String[] args) {
+        Maze3D maze = new Maze3D(3,3,3);
+    }
+}
--- src/maze3D/Maze3D.java
+++ src/maze3D/Maze3D.java
@@ -1,8 +1,82 @@
 package maze3D;
 
+import algorithms.mazeGenerators.Position;
+
 public class Maze3D {
 
-    public int[][][] getMap(){return null;}
-    public Position3D getStartPosition(){return null;}
-    public Position3D getGoalPosition(){return null;}
+
+    private int depth;
+    private int row;
+    private int col;
+    private Position3D startPosition;
+    private Position3D goalPosition;
+    protected int[][][] maze;
+
+
+    public Maze3D(int depth, int row, int col, Position3D startPosition, Position3D goalPosition) {
+        this.row = row;
+        this.col = col;
+        this.depth = depth;
+        this.startPosition = startPosition;
+        this.goalPosition = goalPosition;
+        maze = new int[depth][row][col];
+    }
+
+
+    public Position3D getStartPosition() {
+        return startPosition;
+    }
+
+    public Position3D getGoalPosition() {
+        return goalPosition;
+    }
+
+
+    public int getRows() {
+        return row;
+    }
+
+    public int getCols() {
+        return col;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public int[][][] getMap(){return maze;}
+
+
+    @Override
+    public String toString() {
+        String ret = "{";
+        for (int i = 0; i<depth; i++){
+
+            for (int j=0; j<row; j++) {
+                ret = ret + "{ ";
+
+                for (int k=0; k<col; k++){
+
+                    if (startPosition.toString().equals(i + "," + j)) {
+                        ret = ret + "S" + " ";
+                    } else if (goalPosition.toString().equals(i + "," + j)) {
+                        ret = ret + "E" + " ";
+                    } else {
+                        ret = ret + maze[i][j][k] + " ";
+
+                    }
+                }
+                ret = ret + "}\n";
+            }
+            ret = ret + "-".repeat(col*2+3);
+
+        }
+        ret = ret + "}";
+        return ret;
+    }
+
+
+    public void print() {
+        System.out.println(toString());
+    }
 }
